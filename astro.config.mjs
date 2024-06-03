e53621f1f51538616978a239ce9438f9278cc46a
Agustin Santos <agustinsantos63@gmail.com>
2024-06-03 13:29:12 -0300

    remove themeToggle

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: 'https://astronaut.github.io',
+  base: 'my-repo',
+  integrations: [tailwind()],
+  output: "server",
+  adapter: vercel()
 });
